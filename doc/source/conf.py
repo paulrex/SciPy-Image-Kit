b35904967e7de3902668fd6de5905af534eba827
Mahipal Raythattha <mahipalr@gmail.com>
2009-08-22 19:25:30 -0700

    Sphinx will now successfully generate documentation for the project. We now use the same scipy.css file as the existing online numPy docs. Example doc files for Sphinx input have been placed in doc/source/*.txt.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,11 +18,13 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.append(os.path.abspath('.'))
 
+sys.path.append(os.path.abspath('/Users/mahipal/scipy/scikits.image/'))
+
 # -- General configuration -----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath', 'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -92,6 +94,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 html_theme = 'default'
+html_style = 'scipy.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
